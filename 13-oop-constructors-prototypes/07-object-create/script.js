93e08484d0cd0c4925085830e23560c7db5f6b9a
DevHeris <badairoinioluwa578@gmail.com>
2023-11-08 07:11:39 +0200

    Learnt about another way of createing objects and their prototypes directly

--- 13-oop-constructors-prototypes/07-object-create/script.js
+++ 13-oop-constructors-prototypes/07-object-create/script.js
@@ -1,1 +1,31 @@
- 
+const rectanglePrototypes = {
+  area: function () {
+    return this.width * this.height;
+  },
+  perimeter: function () {
+    return 2 * (this.width + this.height);
+  },
+  isSquare: function () {
+    return this.width === this.height;
+  },
+};
+
+function createRectangle(height, width) {
+  return Object.create(rectanglePrototypes, {
+    height: {
+      value: height,
+    },
+    width: {
+      value: width,
+    },
+  });
+}
+
+const rect = createRectangle(10, 20);
+
+console.log(rect);
+console.log(rect.area());
+console.log(rect.isSquare());
+
+const rect2 = createRectangle(20, 234);
+console.log(rect2.area());
