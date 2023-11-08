6c860d29671c537dce33af314af4737ab5411b20
DevHeris <badairoinioluwa578@gmail.com>
2023-11-08 23:51:59 +0200

    Completed the Static method section and the bind()mehod section

--- 14-oop-classes-private-properties/03-static-methods/script.js
+++ 14-oop-classes-private-properties/03-static-methods/script.js
@@ -4,4 +4,16 @@ class Rectangle {
     this.height = height;
     this.width = width;
   }
+
+  // getClass() {
+  //   return "Rectangle";
+  // }
+
+  static getClass() {
+    return "Rectangle";
+  }
 }
+
+const rect = new Rectangle("Rect 1", 20, 34);
+// console.log(rect.getClass());
+console.log(Rectangle.getClass());
