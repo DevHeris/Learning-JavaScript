cb5e834eeb5da76faac0f14b6f4db2eddae132ea
DevHeris <badairoinioluwa578@gmail.com>
2023-11-09 08:20:01 +0200

    Completed the OOP module

--- 14-oop-classes-private-properties/09-flags-descriptors/script.js
+++ 14-oop-classes-private-properties/09-flags-descriptors/script.js
@@ -2,3 +2,37 @@
 // [[Enumerable]] - if `true`, the property will be returned in a `for...in` loop, otherwise not
 // [[Writable]] - if `true`, the value of the property can be changed, otherwise not
 // [[Value]] - the value of the property
+
+let descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
+console.log(descriptor);
+
+const rectObj = {
+  name: "Rectangle 1",
+  width: 20,
+  height: 30,
+};
+descriptor = Object.getOwnPropertyDescriptor(rectObj, "name");
+console.log(descriptor);
+
+Object.defineProperty(rectObj, "name", {
+  writable: false,
+  configurable: false,
+  enumerable: false,
+});
+
+descriptor = Object.getOwnPropertyDescriptor(rectObj, "name");
+console.log(descriptor);
+
+rectObj.name = "Rect";
+console.log(descriptor); //It refuses to change name at this point
+
+delete rectObj.name; // Wont delete this
+delete rectObj.width; // Actually deletes this
+console.log(rectObj);
+
+for (let [key, value] of Object.entries(rectObj)) {
+  console.log(`${key} - ${value}`);
+}
+
+descriptor = Object.getOwnPropertyDescriptors(rectObj);
+console.log(descriptor);
--- 14-oop-classes-private-properties/10-freezing-sealing-properties/script.js
+++ 14-oop-classes-private-properties/10-freezing-sealing-properties/script.js
@@ -1,3 +1,45 @@
 // Sealing - Prevents properties from being added or removed. Can still be changed.
 
 // Freezing - Prevents properties from being added, removed or changed
+
+const rectObj = {
+  name: "Rectangle 1",
+  width: 10,
+  height: 10,
+};
+
+Object.seal(rectObj);
+
+let descriptors = Object.getOwnPropertyDescriptors(rectObj);
+
+// console.log(descriptors);
+
+rectObj.color = "red"; // Won't be added
+delete rectObj.name; // Won't be removed
+
+rectObj.width = 20; // Will changed
+
+// console.log(rectObj);
+
+const circleObj = {
+  name: "Circle 1",
+  radius: 30,
+};
+
+Object.freeze(circleObj);
+
+descriptors = Object.getOwnPropertyDescriptors(circleObj);
+
+circleObj.color = "blue"; // Won't
+delete circleObj.name; // Won't
+
+circleObj.radius = 13;
+
+console.log(circleObj);
+
+console.log("rectObj is sealed", Object.isSealed(rectObj));
+console.log("rectObj is frozen", Object.isFrozen(rectObj));
+console.log("circleObj is sealed", Object.isSealed(circleObj));
+console.log("circleObj is frozen", Object.isFrozen(circleObj));
+
+// Frozed objects are also sealed
