012b86aa650b7ba0ad4cce89900a4d5600dbcca9
DevHeris <badairoinioluwa578@gmail.com>
2023-11-13 20:23:08 +0200

    installed mini-css-extract-plugin which helps stops the loading of  unstyled html file flashes

--- webpack-starter/webpack.config.js
+++ webpack-starter/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -23,7 +24,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.js$/,
@@ -43,5 +44,6 @@ module.exports = {
       filename: "index.html",
       template: "./src/index.html",
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
