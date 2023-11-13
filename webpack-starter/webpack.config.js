fb39645d548fadad5aa6cf2bab5eed9c1f3dec96
DevHeris <badairoinioluwa578@gmail.com>
2023-11-13 20:07:32 +0200

    Finally completed the  webpack

--- webpack-starter/webpack.config.js
+++ webpack-starter/webpack.config.js
@@ -25,6 +25,16 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
     ],
   },
   plugins: [
