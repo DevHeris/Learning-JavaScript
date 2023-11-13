1f1b666bdcba01be2a53d271e0492fc065d4743e
DevHeris <badairoinioluwa578@gmail.com>
2023-11-13 05:37:42 +0200

    Implemented HTML webpack plugin

--- webpack-starter/webpack.config.js
+++ webpack-starter/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -15,4 +16,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
 };
