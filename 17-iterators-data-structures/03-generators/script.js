21b19c16b11591bf6f616be09ce0afa48fad040c
DevHeris <badairoinioluwa578@gmail.com>
2023-11-20 09:42:59 +0200

    learnt how to use generators to create iterators the easier way

---
+++ 17-iterators-data-structures/03-generators/script.js
@@ -0,0 +1,24 @@
+function* createTeamIterator(teams) {
+  for (let i = 0; i < teams.length; i++) {
+    yield teams[i];
+  }
+}
+
+const teams = ["Red Sox", "Yankees", "Astros", "Dogers"];
+
+const iterator = createTeamIterator(teams);
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+
+for (const team of createTeamIterator(teams)) {
+  console.log(team);
+}
+
+console.log([...createTeamIterator(teams)]);
+
+const [first, second, third] = createTeamIterator(teams);
+
+console.log(first, second, third);
